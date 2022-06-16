dce2c90a2145a2c15e51ecf14b7d9ce6dd2f8c48
Estebes <estebesm95@gmail.com>
2022-06-16 15:17:00 +0600

    nothing

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
-const {merge} = require('webpack-merge')
-const pug = require('./webpack/pug')
-const devServer = require('./webpack/devServer')
-const sass = require('./webpack/sass')
+const path = require('path');
+const {merge} = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devServer = require('./webpack/devServer');
+const sass = require('./webpack/sass');
+const images = require('./webpack/images');
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const fs = require("fs");
@@ -11,23 +12,23 @@ const PATHS = {
     source: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'build')
 }
-const PAGES_DIR = `${path.resolve(__dirname, "src")}/pages/`
-const PAGES_DIRS = fs.readdirSync(PAGES_DIR).map(dir => `${path.resolve(__dirname, "src")}/pages/${dir}`)
+const PAGES_DIR = `${path.resolve(__dirname, "src")}/pages/`;
+const PAGES_DIRS = fs.readdirSync(PAGES_DIR).map(dir => `${path.resolve(__dirname, "src")}/pages/${dir}`);
 
 const PAGES = PAGES_DIRS.map(dir => ({
     filenames: fs.readdirSync(dir).filter(filename => filename.endsWith(".pug")),
     path: dir
-}))
+}));
 
-const res = []
+const res = [];
 
 PAGES.forEach(page => page.filenames.forEach(filename => {
     res.push( new HtmlWebpackPlugin({
         template: `${page.path}/${filename}`,
         chunks: [filename.substring(0, filename.length-4)],
         filename: `${filename.replace(/\.pug/, '.html')}`
-    }))
-}))
+    }));
+}));
 
 const common = merge([
     {
@@ -44,18 +45,19 @@ const common = merge([
             ...res
         ],
     },
-    pug()
-])
+    pug(),
+    sass(),
+    images()
+]);
 
 module.exports = function (env, argv){
     if(argv.mode === "production"){
-        return common
+        return common;
     }
     if(argv.mode === "development"){
         return merge([
             common,
-            devServer(),
-            sass()
-        ])
+            devServer()
+        ]);
     }
 }
